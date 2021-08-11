a35ec01c62bfb006865a234549a5ddce450b78ab
Crowdbotics <team@crowdbotics.com>
2021-08-11 04:29:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "withered_bread_29710Identifier",
+  url: "https://withered_bread_29710Identifier.botics.co",
+  api: "https://withered_bread_29710Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
